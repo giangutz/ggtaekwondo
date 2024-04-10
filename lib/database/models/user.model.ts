92f8e1c87ba204226294db288c84c54e6edd2705
rr106544 <gian_gutierrez@dlsu.edu.ph>
2024-04-10 14:49:56 +0800

    Update user.model.ts

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -2,7 +2,7 @@ import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
-  userType: { type: String, required: true },
+//   userType: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
