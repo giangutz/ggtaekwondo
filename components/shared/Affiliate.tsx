ca8055dc848ebcdb08f2be6e9e0eb787f1089dbc
giangutz <80666756+giangutz@users.noreply.github.com>
2024-05-22 16:07:22 +0800

    optimized images

--- components/shared/Affiliate.tsx
+++ components/shared/Affiliate.tsx
@@ -1,5 +1,7 @@
 import { companies } from "@/constants";
+import Image from "next/image";
 import React from "react";
+
 const Affiliate = () => {
   return (
    <div className="p-4">
@@ -10,9 +12,11 @@ const Affiliate = () => {
   <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16">
     {companies.map((company) => (
       <div key={company.id} className="flex md:max-w-60 max-w-32 gap-4 p-4 transition-transform duration-200 hover:scale-105">
-        <img
+        <Image
           src={company.img}
           alt={company.name}
+          width={600}
+          height={1000}
           className={`md:w-24 w-20 ${[3, 4, 5].includes(company.id) ? "md:w-32 w-28" : ""}`}
         />
       </div>
