3dd9266d0b73cf3d2cb53bfd187d66ad64f78fe5
rr106544 <gian_gutierrez@dlsu.edu.ph>
2024-06-04 13:54:43 +0800

    added prettier and bun

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image';
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 export default async function NotFound() {
   return (
-    <main className="h-screen w-full flex flex-col justify-center items-center bg-white">
+    <main className="flex h-screen w-full flex-col items-center justify-center bg-white">
       <Image
         src="/assets/images/logo.png"
         width={60}
@@ -12,21 +12,21 @@ export default async function NotFound() {
         alt="Grit & Glory Taekwondo Logo"
         className="pb-8"
       />
-      <h1 className="text-9xl font-extrabold text-black tracking-widest">
+      <h1 className="font-extrabold text-9xl tracking-widest text-black">
         404
       </h1>
-      <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
+      <div className="absolute rotate-12 rounded bg-[#FF6A3D] px-2 text-sm">
         Page Not Found
       </div>
       <button className="mt-5">
         {/* <a className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
           <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span> */}
 
-          <span className="relative block px-8 py-3 bg-white border border-black text-black transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px] rounded-md">
-            <Link href="/">Go Home</Link>
-          </span>
+        <span className="relative block rounded-md border border-black bg-white px-8 py-3 text-black shadow-[3px_3px_0px_black] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none">
+          <Link href="/">Go Home</Link>
+        </span>
         {/* </a> */}
       </button>
     </main>
   );
-};
+}
